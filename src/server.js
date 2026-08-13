3687427d949b9309a8a299935f2fbe0bb5730729
shagun <shagunraghav753@gmail.com>
2026-08-13 18:19:46 +0530

    Updated project

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-
+require("dotenv").config();
 const express = require("express");
+ const connectdb= require("./Config/database");
+
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
 
-app.listen(3000, () => {
+
+connectdb().then(()=>{
+    console.log("database connected");
+    app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+}).catch(err =>{
+    console.error("database not connected");
+})
+
+
