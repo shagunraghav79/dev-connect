0968984b9a3c68864975404184614f5dce163b09
shagun <shagunraghav753@gmail.com>
2026-08-14 16:53:15 +0530

    Updated project

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,10 @@
 const mongoose=require('mongoose');
 
 const userSchema= new mongoose.Schema({
-    firstName:{type:String},
-    lastName:{type:String}
-})
+    firstName:{type:String , required:true},
+    lastName:{type:String},
+    emainId:{type:String , required:true}
+}) 
 
 const UserModel= mongoose.model("user",userSchema);
 module.exports=UserModel;
